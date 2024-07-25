dfec260242cbed8c2f77cedd14385ad65c5c2f4f
Elliot Nunn <elliotnunn@me.com>
2024-07-25 10:40:46 +0800

    Early return from printf when logenable off

--- printf.c
+++ printf.c
@@ -897,6 +897,7 @@ static int _vsnprintf(out_fct_type out, char* buffer, const size_t maxlen, const
 
 int printf_(const char* format, ...)
 {
+  if (!logenable) return 0;
   va_list va;
   va_start(va, format);
   char buffer[1];
